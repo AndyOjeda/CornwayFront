51d421e32e9488ba3110feefdb467ec54428587c
Andres Ojeda <andyojeda2003@gmail.com>
2024-05-18 22:19:52 -0500

    good commit message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { RegisterPageComponent } from './register-page/register-page.component'
 import { CornwayComponent } from './LandingPage/cornway/cornway.component';
 import { CosechaComponent } from './LandingPage/cosecha/cosecha.component';
 import { CultivosComponent } from './LandingPage/cultivos/cultivos.component';
-import { ConfiguracionComponent } from './LandingPage/configuracion/configuracion.component';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
 
 
 export const routes: Routes = [
